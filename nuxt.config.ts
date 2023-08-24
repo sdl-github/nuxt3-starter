f0b5a0486323ae487dc0d6b1891de6a644a9f9e3
sdl-code <sdl-life@foxmail.com>
2023-08-24 13:47:44 +0800

    fix antd css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,10 +13,10 @@ export default defineNuxtConfig({
   experimental: {
     // when using generate, payload js assets included in sw precache manifest
     // but missing on offline, disabling extraction it until fixed
-    payloadExtraction: false,
-    inlineSSRStyles: false,
-    renderJsonPayloads: true,
-    typedPages: true,
+    // payloadExtraction: false,
+    // inlineSSRStyles: false,
+    // renderJsonPayloads: true,
+    // typedPages: true,
   },
 
   css: [
@@ -32,11 +32,6 @@ export default defineNuxtConfig({
       options: {
         target: 'esnext',
       },
-    },
-    prerender: {
-      crawlLinks: false,
-      routes: ['/'],
-      ignore: ['/hi'],
     },
   },
 
